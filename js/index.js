d97d3d1e3b9d6f0e50765d730e13d82ad04a9a28
trig0 <allengordon0@gmail.com>
2016-12-13 11:41:23 -0600

    lunch

--- js/index.js
+++ js/index.js
@@ -1,30 +1,39 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import * as actions from './actions/index'
 
 import store from './store'
 
-console.log(store);
+import Game from './components/game'
+
+// console.log(store);
 
 //learn about this
 // subscribe is a method that listens for changes on the store
-let unsubscribe = store.subscribe(() => {
-    console.log(store.getState());
-}) 
+// let unsubscribe = store.subscribe(() => {
+//     console.log(store.getState());
+// })
+//
+//
+// 	store.dispatch(actions.newGame())
+// 	store.dispatch(actions.addGuess(95))
+// 	store.dispatch(actions.addGuess(85))
+// 	store.dispatch(actions.addGuess(75))
+// 	store.dispatch(actions.addGuess(65))
+// 	store.dispatch(actions.addGuess(55))
+// 	store.dispatch(actions.addGuess(45))
+// 	store.dispatch(actions.addGuess(35))
+// 	store.dispatch(actions.addGuess(25))
+// 	store.dispatch(actions.addGuess(15))
+// 	store.dispatch(actions.addGuess(5))
+// 	store.dispatch(actions.newGame())
+//
+//
+// unsubscribe();
 
-
-	store.dispatch(actions.newGame())
-	store.dispatch(actions.addGuess(95))
-	store.dispatch(actions.addGuess(85))
-	store.dispatch(actions.addGuess(75))
-	store.dispatch(actions.addGuess(65))
-	store.dispatch(actions.addGuess(55))
-	store.dispatch(actions.addGuess(45))
-	store.dispatch(actions.addGuess(35))
-	store.dispatch(actions.addGuess(25))
-	store.dispatch(actions.addGuess(15))
-	store.dispatch(actions.addGuess(5))
-	store.dispatch(actions.newGame())
-
-
-unsubscribe();
+document.addEventListener('DOMContentLoaded', () =>
+  ReactDOM.render(<Game />, document.getElementById('app')
+  );
+);
