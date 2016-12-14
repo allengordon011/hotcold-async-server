e163cd2cddfad1b08eee648c8aee4efba996c8aa
trig0 <allengordon0@gmail.com>
2016-12-14 15:13:01 -0600

    building tests

--- test/test-index.js
+++ test/test-index.js
@@ -10,19 +10,53 @@ import Feedback from '../js/components/feedback';
 import GuessCount from '../js/components/guess-count';
 // import GuessInput from '../js/components/guess-input';
 // import NavBar from '../js/components/nav-bar';
-// import PrevGuesses from '../js/components/prev-guesses';
+import PrevGuesses from '../js/components/prev-guesses';
 
+const renderer = TestUtils.createRenderer();
 
 describe('GuessCount Component', function() {
-  it('it renders a p with guess count',  function() {
-  	const renderer = TestUtils.createRenderer();
+  it('renders a p with guess count',  function() {
+  	// const renderer = TestUtils.createRenderer();
   	renderer.render(<GuessCount stateCount={4}/>)
 
   	const result = renderer.getRenderOutput();
-  	console.log(result);
+  	// console.log(result);
 
-  	result.type.should.equal('p') 
+  	result.type.should.equal('p')
   	result.props.children.join('').should.equal('Guess #4!')
 
+  });
+});
+describe('Feedback Component', function() {
+  it('renders a feedback div with current feedback', function(){
+    renderer.render(<Feedback stateFeedback='hot'/>)
+
+    const result = renderer.getRenderOutput();
+    // console.log(result);
+
+    result.type.should.equal('div')
+    result.props.id.should.equal('feedback')
+    result.props.children.should.equal('hot')
+  });
+});
+
+describe.only('PrevGuesses Component', function(){
+  it('renders a prev-guesses div with a mapped list', function() {
+    renderer.render(<PrevGuesses guessedNumbers={[1, 2, 3]} />)
+
+    const result = renderer.getRenderOutput();
+    // console.log(result);
+    // console.log(result.props.children.props);
+    console.log(result.props.children.props.children);
+
+    result.type.should.equal('div')
+    result.props.id.should.equal('prev-guesses')
+    result.props.children.type.should.equal('ul')
+    result.props.children.props.children[0].type.should.equal('li')
+    result.props.children.props.children[1].type.should.equal('li')
+    result.props.children.props.children[2].type.should.equal('li')
+    result.props.children.props.children[0].props.children.should.equal(1)
+    result.props.children.props.children[1].props.children.should.equal(2)
+    result.props.children.props.children[2].props.children.should.equal(3)
   });
 });
